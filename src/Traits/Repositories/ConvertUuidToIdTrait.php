7faf440791d242668178363d077a81a1b85f4c5b
Daniel S.F Silva <daniel.felix05@gmail.com>
2019-03-21 01:51:09 -0300

    Adding new Trait very useful for Repositories on models that uses UUID and ID on same table

---
+++ src/Traits/Repositories/ConvertUuidToIdTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+/**
+ * Trait ConvertUuidToIdTrait
+ * @package App\Traits\Repositories only use this specific trait inside repository class
+ */
+Trait ConvertUuidToIdTrait
+{
+  /**
+   * @param string $uuid
+   * @param bool $getModel if true will return the Eloquent object, otherwise only ID number is returned, default false
+   * @return mixed
+   */
+    public function getInternalId(string  $uuid, bool $getModel=false)
+    {
+        $model = $this->model()::where('uuid', $uuid)->first();
+        if($getModel)
+          return $model;
+
+        return $model->id;
+    }
+}
