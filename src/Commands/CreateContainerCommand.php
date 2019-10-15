4f8595c0991b69722286b0ecd85eb03a1be82d88
Daniel S.F Silva <daniel.felix05@gmail.com>
2019-10-15 18:48:22 -0300

    Add ApiAto core and Updates
     Added apiAtoCore, composer-merge-plugin and CreateApiRequestCommand

--- src/Commands/CreateContainerCommand.php
+++ src/Commands/CreateContainerCommand.php
@@ -23,26 +23,38 @@ class CreateContainerCommand extends Command
       'Actions',
       'Models',
       'Tasks',
+      'Traits',
+      'Jobs',
       'Data',
-      'Data/Seeders',
-      'Data/Factories',
-      'Data/Criterias',
-      'Data/Repositories',
-      'Data/Validators',
-      'Data/Transporters',
-      'Data/Migrations',
+      'Data\\Seeders',
+      'Data\\Factories',
+      'Data\\Criterias',
+      'Data\\Repositories',
+      'Data\\Validators',
+      'Data\\Transporters',
+      'Data\\Migrations',
+      'Data\\Validators',
+      'Data\\Rules',
       'Events',
       'Listeners',
+      'Notifications',
+      'Providers',
+      'Mails',
+      'Mails\\Templates',
       'Tests',
-      'Tests/Unit',
-      'Tests/Traits',
+      'Tests\\Unit',
+      'Tests\\Traits',
       'UI',
-      'UI/API/Controllers',
-      'UI/API/Presenters',
-      'UI/API/Repositories',
-      'UI/API/Requests',
-      'UI/API/Routes',
-      'UI/API/Transformers',
+      'UI\\API\\Controllers',
+      'UI\\API\\Presenters',
+      'UI\\API\\Requests',
+      'UI\\API\\Routes',
+      'UI\\API\\Transformers',
+      'UI\\API\\Tests',
+      'UI\\API\\Tests\\Functional',
+      'UI\\CLI',
+      'UI\\CLI\\Commands',
+      'UI\\CLI\\Routes',
     ];
 
     /**
@@ -63,7 +75,7 @@ class CreateContainerCommand extends Command
     public function handle()
     {
       $name = $this->argument('name');
-      $root = base_path() . '/app/Containers/'.$name . '/';
+      $root = base_path() . '\\app\\Containers\\'.$name . '\\';
 
 
       if (! is_dir($root)) {
--- src/Providers/MolezinhaServiceProvider.php
+++ src/Providers/MolezinhaServiceProvider.php
@@ -3,48 +3,36 @@
 namespace Molezinha\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Molezinha\Commands\CreateContainerCommand;
+use Molezinha\Commands\CreateMigrationCommand;
+use Molezinha\Commands\CreateModelCommand;
+use Molezinha\Commands\CreateApiRequestCommand;
 use Molezinha\Core\Molezinha;
-use Molezinha\Traits\Loaders\AutoLoaderTrait;
 
 /**
  * Class MolezinhaServiceProvider
  * @package Molezinha\Providers
  *
- * Inspired by : https://github.com/apiato/core/blob/master/Providers/ApiatoProvider.php
- *  and https://github.com/santigarcor/laratrust/blob/master/src/LaratrustServiceProvider.php
+ * Inspired by : https://github.com/apiato/core/blob/master/Generator/GeneratorsServiceProvider.php
+ *
  */
 class MolezinhaServiceProvider extends ServiceProvider
 {
-  use AutoLoaderTrait;
-  /**
-   * The commands to be registered.
-   *
-   * @var array
-   */
-  protected $commands = [
-    'CreateContainer' => 'command.molezinha.createcontainer',
-    'CreateModel' => 'command.molezinha.createmodel',
-    'CreateMigration' => 'command.molezinha.createmigration'
-
-  ];
-
 
   public function boot()
   {
-    $this->publishes([
-      __DIR__.'/../Config/molezinha.php' => config_path('molezinha.php'),
-    ]);
-
-    //Load Containers and Ship Components
-    $this->runBootLoader();
-
-
+      //
   }
 
   public function register()
   {
     $this->registerMolezinha();
-    $this->registerCommands();
+    $this->registerCommands([
+        CreateMigrationCommand::class,
+        CreateContainerCommand::class,
+        CreateModelCommand::class,
+        CreateApiRequestCommand::class
+    ]);
 
   }
 
@@ -62,53 +50,20 @@ class MolezinhaServiceProvider extends ServiceProvider
     $this->app->alias('molezinha', 'Molezinha\Core\Molezinha');
   }
 
-  /**
-   * Register the given commands.
-   *
-   * @return void
-   */
-  protected function registerCommands()
-  {
-    foreach (array_keys($this->commands) as $command) {
-      $method = "register{$command}Command";
-
-      call_user_func_array([$this, $method], []);
+    /**
+     * Register the commandsrising.
+     * @param array $classes
+     */
+    protected function registerCommands(array $classes)
+    {
+        foreach ($classes as $class)
+        {
+            $lowerClass = strtolower($class);
+            $this->app->singleton("command.molezinha.$lowerClass", function ($app) use ($class)
+            {
+                return $app[$class];
+            });
+            $this->commands("command.molezinha.$lowerClass");
+        }
     }
-
-    $this->commands(array_values($this->commands));
-  }
-
-  /**
-   * Get the services provided.
-   *
-   * @return array
-   */
-  public function provides()
-  {
-    return array_values($this->commands);
-  }
-
-  protected function registerCreateContainerCommand()
-  {
-    $this->app->singleton('command.molezinha.createcontainer', function () {
-      return new \Molezinha\Commands\CreateContainerCommand();
-    });
-  }
-
-  protected function registerCreateModelCommand()
-  {
-    $this->app->singleton('command.molezinha.createmodel', function ($app) {
-      return new \Molezinha\Commands\CreateModelCommand($app['files']);
-    });
-  }
-
-  protected function registerCreateMigrationCommand()
-  {
-    $this->app->singleton('command.molezinha.createmigration', function () {
-      return new \Molezinha\Commands\CreateMigrationCommand();
-    });
-  }
-
-
-
 }
