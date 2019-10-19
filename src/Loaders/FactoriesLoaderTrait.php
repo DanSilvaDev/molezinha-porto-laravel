70c6b212732a318b738db0e50ba9259cf26bc70b
Daniel S.F Silva <daniel.felix05@gmail.com>
2019-10-19 16:21:45 -0300

    Fixed Factory Register Behavior
      Changed and fixed factory behavior, now its lighter and only adds to the original factory the Containers Directory, doesnt overwrite anymore

--- src/Loaders/FactoriesLoaderTrait.php
+++ src/Loaders/FactoriesLoaderTrait.php
@@ -5,31 +5,46 @@ namespace Molezinha\Loaders;
 use App;
 use Faker\Generator;
 use Illuminate\Database\Eloquent\Factory;
+use Molezinha\Core\Facades\Molezinha;
 
 /**
  * Class FactoriesLoaderTrait.
- *
- * @author  Mahmoud Zalt <mahmoud@zalt.me>
  */
 trait FactoriesLoaderTrait
 {
 
     /**
      * By default Laravel takes a shared factory directory to load from it all the factories.
-     * This function add the containers path and also the Laravel Default One, so
-     * new implementations or installations of this package won't be impacted.
-     */
+     * This function just add the containers directories, not removing the original ones
+     **/
     public function loadFactoriesFromContainers()
     {
         $loadersDirectory = str_replace(getcwd(), '', __DIR__);
+        $newFactoriesPath = $this->buildFactoriesContainersPath();
+        /** @var  $factoryClass Factory */
+        $factoryClass = $this->app->make(Factory::class);
+        // Load in the original Factory all the containers
+        foreach ($newFactoriesPath as $index => $path)
+        {
+            $factoryClass->load($path);
+        }
+    }
 
-        $newFactoriesPath = $loadersDirectory . '/FactoryMixer';
+    private function buildFactoriesContainersPath()
+    {
+        $containersFactoriesPath = '/Data/Factories/';
+        $paths = [];
 
-        App::singleton(Factory::class, function ($app) use ($newFactoriesPath) {
-            $faker = $app->make(Generator::class);
-
-            return Factory::construct($faker, base_path() . $newFactoriesPath);
-        });
+        foreach (Molezinha::getContainersNames() as $containerName)
+        {
+            $containersDirectory = base_path('app/Containers/' . $containerName . $containersFactoriesPath);
+            // Check if it's a valid Directory
+            if (\File::isDirectory($containersDirectory))
+            {
+                $paths[] = $containersDirectory;
+            }
+        }
+        return $paths;
     }
 
 }
